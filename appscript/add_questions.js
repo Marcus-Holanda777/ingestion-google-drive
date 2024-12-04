692c8e7303b25a9791226389938c740f138a25a6
Marcus Holanda <mvsh777@hotmail.com>
2024-12-04 14:04:12 -0300

    appscript

--- appscript/add_questions.js
+++ appscript/add_questions.js
@@ -1,49 +1,69 @@
 class SheetService {
-    constructor(id_plan) {
-       this.id_plan = SpreadsheetApp.openById(id_plan);
-    }
-  
-    getValues(name_plan, range) {
-      // Retorna um array bidimensiona
-      // com base em um intervalo da planilha
-      // se o tamnho do array for 0 retornar um array vazio
-  
-      const sheet = this.id_plan.getSheetByName(name_plan);
-      const values = sheet.getRange(range).getValues();
-  
-      return values.length > 0 ? values: [[]]; // condicao ternaria JS
-    }
+  constructor(idPlan) {
+     this.appSheet = SpreadsheetApp.openById(idPlan);
   }
-  
-  class FormService {
-    constructor(form) {
-      this.form = form || FormApp.getActiveForm();
-    }
-  
-    getQuestion(id_question) {
-      return this.form.getItems()[id_question];
-    }
-  
-    setUpdateList(id_question, values) {
-      const question = this.getQuestion(id_question).asListItem();
-      const values_options = [...new Set(values.map(([row]) => row))].sort();
-  
-      question.setChoiceValues(values_options);
-    }
+
+  getValues(name, range) {
+    // Retorna um array bidimensiona
+    // com base em um intervalo da planilha
+    // se o tamnho do array for 0 retornar um array vazio
+
+    const sheet = this.appSheet.getSheetByName(name);
+    const values = sheet.getRange(range).getValues();
+
+    return values.length > 0 ? values: [[]]; // condicao ternaria JS
   }
-  
-  function dropdownList() {
-    const plan = new SheetService(
-        "id_planilha"
-    )
+}
+
+class FormService {
+  constructor(form) {
+    this.form = form || FormApp.getActiveForm();
+  }
+
+  getQuestion(question) {
+    return this.form.getItems()[question];
+  }
+
+  setUpdateList(question, values, convert = 'list') {
+    const includesConvert = ["list", "mult"];
+
+    if(!includesConvert.includes(convert)) {
+      throw new Error("Option not allowed !");
+    }
+
+    if(values.length === 0 || values[0].length === 0) {
+      throw new Error("Empty list !");
+    }
     
-    const id_form = FormApp.openById(
-      "id_forms"
-    )
+    let appQuestion = this.getQuestion(question)
     
-    // adiciona a lista de valores a uma questão do google forms
-    const form = new FormService(id_form);
-    form.setUpdateList(0, plan.getValues("estados", "A2:A"));
-    form.setUpdateList(2, plan.getValues("cargo", "A2:A"));
+    if(convert === 'list') {
+      appQuestion = appQuestion.asListItem();
+    } else {
+      appQuestion = appQuestion.asCheckboxItem();
+    }
+
+    const options = [...new Set(values.map(([row]) => row))].sort();
+    appQuestion.setChoiceValues(options);
   }
+}
+
+function dropdownList() {
+  const plan = new SheetService(
+      "id_plan"
+  )
   
+  const id_form = FormApp.openById(
+    "id_form"
+  )
+  
+  const form = new FormService(id_form);
+
+  // lista suspensa
+  form.setUpdateList(0, plan.getValues("estados", "A2:A"));
+  form.setUpdateList(2, plan.getValues("cargo", "A2:A"));
+
+  // selecao multipla
+  form.setUpdateList(3, plan.getValues("conhecimentos", "A2:A",), "mult");
+  form.setUpdateList(4, plan.getValues("ferramentas", "A2:A"), "mult");
+}
