5c16e64afa0544297b1397ab1bec6e2a86f39301
Marcus Holanda <mvsh777@hotmail.com>
2024-12-09 23:28:26 -0300

    cloud build2

--- terraform/run-services.tf
+++ terraform/run-services.tf
@@ -1,12 +1,12 @@
-resource "google_project_iam_member" "cloud_run_admin" {
-  project = var.project_id
-  role    = "roles/run.admin"
-  member  = "serviceAccount:${google_service_account.pesquisa_account.email}"
-}
-
 resource "google_project_iam_member" "cloud_build_editor" {
   project = var.project_id
   role    = "roles/cloudbuild.builds.editor"
+  member  = "serviceAccount:${google_service_account.pesquisa_account.email}"
+}
+
+resource "google_project_iam_member" "cloud_run_admin" {
+  project = var.project_id
+  role    = "roles/run.admin"
   member  = "serviceAccount:${google_service_account.pesquisa_account.email}"
 }
 
@@ -19,6 +19,12 @@ resource "google_project_iam_member" "service_account_user" {
 resource "google_project_iam_member" "artifact_registry_admin" {
   project = var.project_id
   role    = "roles/artifactregistry.admin"
+  member  = "serviceAccount:${google_service_account.pesquisa_account.email}"
+}
+
+resource "google_project_iam_member" "gcr_storage_admin" {
+  project = var.project_id
+  role    = "roles/storage.admin"
   member  = "serviceAccount:${google_service_account.pesquisa_account.email}"
 }
 
